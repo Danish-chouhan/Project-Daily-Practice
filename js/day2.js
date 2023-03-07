455eb6bc89ab70672454e60c2ad00c606d5eaaf5
Danish-chouhan <danishchouhan41@gmail.com>
2023-03-08 01:27:16 +0530

    Day-2

--- js/day2.js
+++ js/day2.js
@@ -1,63 +1,70 @@
-function giveMeSomething(str){
- return ( 'something '+str);
-//  return (`something ${str}`);
-   //  return 'something'.concat( ' ' ,str);
-}
- console.log( giveMeSomething("is better than nothing") );
- console.log( giveMeSomething("Bob Jane"));
- console.log( giveMeSomething("something") );
+// -------------------------------------
 
-function triArea(base,height){
-   return (base * height) / 2;
+function giveMeSomething(str) {
+    return `something ${str}`
 }
 
-// `Area of a Triangle`
- console.log( triArea(3, 2));
- console.log( triArea(7, 4) );
- console.log( triArea(10, 10) );
+// console.log(giveMeSomething("is better than nothing")) // "something is better than nothing"
+// console.log(giveMeSomething("Bob Jane")) // "something Bob Jane"
+// console.log(giveMeSomething("something")) // "something something"
 
-function howManySeconds(hour){
-     return 3600 * hour
+// -------------------------------------
+
+// Area of a Triangle`
+function triArea(base, height) {
+    return 1 / 2 * (base * height)
 }
 
-  console.log(howManySeconds(2));
-  console.log(howManySeconds(10));
-  console.log(howManySeconds(24));
- 
- function getFirstValue(arr){
-    // return arr.shift()
-    return arr[0]
- }
- console.log(getFirstValue([1, 2, 3]))
- console.log(getFirstValue([80, 5, 100]))
- console.log(getFirstValue([-500, 0, 50]))
+// console.log(triArea(3, 2)) // 3
+// console.log(triArea(7, 4)) // 14
+// console.log(triArea(10, 10)) // 50
+// 1/2 × b × h
 
-function nextEdge(num1,num2){
-    return Number(num1) + Number(num2)-1
-   //  return (num1-1) + (num2-1) + 2 -1
-   //  return (num1*1) + (num2*1) -1
-   //  return parseInt(num1) + parseInt(num2)-1
-   //  return parseFloat(num1) + parseFloat(num2)-1
-   //  return (num1/1) + (num2/1) -1
-   //  return +(num1) + +(num2) -1
+
+// -------------------------------------
+function howManySeconds(hours) {
+    return 3600 * hours
 }
 
- console.log(nextEdge(8, 10) )
- console.log(nextEdge(5, 7) )
- console.log(nextEdge(9, 2) )
+// console.log(howManySeconds(2) )// 7200
+// console.log(howManySeconds(10)) // 36000
+// console.log(howManySeconds(24)) // 86400
 
- function findPerimeter(num1,num2){
-    return 2 * (Number(num1) + Number(num2)) ;
-    
- }
- console.log(findPerimeter(6, 7))
- console.log(findPerimeter(20, 10))
- console.log(findPerimeter(2, 9))
+// -------------------------------------
+
+const getFirstValue = (arr => arr[0])
+
+// console.log(getFirstValue([1, 2, 3]) )// 1
+// console.log(getFirstValue([80, 5, 100]) )// 80
+// console.log(getFirstValue([-500, 0, 50]) )// -500
+
+// -------------------------------------
+
+function nextEdge(num1, num2) {
+    return (num1 + num2) - 1;
+}
+
+// console.log(nextEdge(8, 10)) // 17
+// console.log(nextEdge(5, 7)) // 11
+// console.log(nextEdge(9, 2)) // 10
+
+// -------------------------------------
+function findPerimeter(num1, num2) {
+    return 2 * (num1 + num2)
+}
+
+// console.log(findPerimeter(6, 7)  )// 26
+// console.log(findPerimeter(20, 10)) // 60
+// console.log(findPerimeter(2, 9)  )// 22
+
+// -------------------------------------
 
 function remainder(num1,num2){
-   return num1 % num2
+    return num1 % num2
 }
- console.log(remainder(1, 3))
- console.log(remainder(3, 4))
- console.log(remainder(-9, 45))
- console.log(remainder(5, 5))
+
+// console.log(remainder(1, 3) )// 1
+// console.log(remainder(3, 4) )// 3
+// console.log(remainder(-9, 45) )// -9
+// console.log(remainder(5, 5) )// 0
+// -------------------------------------
